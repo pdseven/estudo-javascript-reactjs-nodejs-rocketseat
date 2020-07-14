ebce077c7fc0e6bf71baaff6083e906e4a80be5d
Diego Neves <pdcassiano@gmail.com>
2020-07-14 18:52:36 -0300

    inicio do projeto

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (resquest, response) => {
+    return response.json({ message: 'Hello World' })
+})
+
+app.listen(3333, () => {
+    console.log('🚀 Back-end started!')
+})
