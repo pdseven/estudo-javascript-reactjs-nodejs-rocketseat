333bb8983bfe186d37f44aa0a65d41f3b1b96cd7
Diego Neves <pdcassiano@gmail.com>
2020-07-27 17:22:41 -0300

    conculido o front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
+
+import './App.css'
+// import backgroundImage from './assets/background.jpg'
 
 import Header from './components/Header'
 
 function App() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
+
+    async function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`)
+
+        // setProjects([...projects, `Novo projeto ${Date.now()}`])
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Diego Neves"
+        })
+
+        const project = response.data
+        setProjects([...projects, project])
+    }
+
     return (
         <>
-            <Header />
-            <Header />
+            <Header title="Projects" />
+            {/* <img width={300} src={backgroundImage}/> */}
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     )
 }
