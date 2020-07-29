1821dbf4be83aef0b731c8c04dd56c2789e17050
Diego Neves <pdcassiano@gmail.com>
2020-07-29 09:07:27 -0300

    configurado editor config

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,8 +3,9 @@ import express from 'express'
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({message: 'hello GoStack'})
+  return response.json({ message: 'hello GoStack' })
 })
 app.listen(3333, () => {
-    console.info('Server started on port 3333!')
+  console.info('Server started on port 3333!')
 })
+ 
