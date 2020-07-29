767fe9af79e3cc2062e00a7a5b2c9ed19de0921f
Diego Neves <pdcassiano@gmail.com>
2020-07-29 09:30:21 -0300

    configurando o eslint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello GoStack' })
-})
+app.get('/', (request, response) => response.json({ message: 'hello GoStack' }));
+
 app.listen(3333, () => {
-  console.info('Server started on port 3333!')
-})
- 
+  console.info('Server started on port 3333!');
+});
