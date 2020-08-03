98ddb214978cc8c1cab68193f000812a16107f69
Diego Neves <pdcassiano@gmail.com>
2020-08-03 11:40:06 -0300

    criado o model User

--- iniciando-back-end/src/database/migrations/1596462780993-CreateUsers.ts
+++ iniciando-back-end/src/database/migrations/1596462780993-CreateUsers.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import {MigrationInterface, QueryRunner, Table, Timestamp } from 'typeorm';
 
-export default class CreateAppointments1596118396371 implements MigrationInterface {
+export default class CreateUsers1596462780993 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +15,18 @@ export default class CreateAppointments1596118396371 implements MigrationInterfa
             default: 'uuid_generate_v4()'
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
+            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -38,7 +44,7 @@ export default class CreateAppointments1596118396371 implements MigrationInterfa
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 
 }
